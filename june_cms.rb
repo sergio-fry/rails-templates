219c3e83e4808c63673ed7a42b2bf3e755a32737
Sergei O. Udalov <sergei.udalov@gmail.com>
2010-10-13 15:18:44 +0400

    routing

--- june_cms.rb
+++ june_cms.rb
@@ -13,14 +13,28 @@ tags
 tmp/**/*
 END
 
-rake "db:migrate"
 
 # JuneCMS
 git :submodule => "add ssh://git@linode1/home/git/june-cms.git vendor/plugins/june-cms"
 run "git submodule update --recursive"
-rake "rake june_cms:sync"
-rake "rake june_cms:add_migration"
+rake "june_cms:sync"
+rake "june_cms:add_migration"
+rake "db:migrate"
+rake "june_cms:restore_content_unit_templates"
 
+
+generate :controller, "Welcome index"
+
+# Routing
+file "config/routes.rb", <<-END
+ActionController::Routing::Routes.draw do |map|
+  map.root :controller => :welcome
+
+  JuneCMS::Routes.draw(map)
+end
+END
+
+# Plugins
 plugin "fantom_controls", :git => "git@github.com:sergio-fry/fantom_controls.git", :submodule => true
 plugin "Simple-nicEdit", :git => "git@github.com:sergio-fry/Simple-nicEdit.git", :submodule => true
 
