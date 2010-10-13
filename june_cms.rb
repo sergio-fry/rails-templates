b08977e4e7fbdb0bea94407fef93d4938495a4ba
Sergei O. Udalov <sergei.udalov@gmail.com>
2010-10-13 16:35:34 +0400

    fcuk

--- june_cms.rb
+++ june_cms.rb
@@ -75,7 +75,7 @@ END
 
 # Plugins
 plugin "fantom_controls", :git => "git@github.com:sergio-fry/fantom_controls.git", :submodule => true
-arun "ruby script/runner vendor/plugins/fantom_controls/install.rb"
+run "ruby script/runner vendor/plugins/fantom_controls/install.rb"
 
 plugin "Simple-nicEdit", :git => "git@github.com:sergio-fry/Simple-nicEdit.git", :submodule => true
 run "ruby script/runner vendor/plugins/Simple-nicEdit/install.rb"
