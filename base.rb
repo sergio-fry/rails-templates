7920462ad9bd107316ff06cd1dbbef44d2fe7cf6
Sergei O. Udalov <sergei.udalov@gmail.com>
2011-05-03 22:01:01 +0400

    it works?

--- base.rb
+++ base.rb
@@ -1,15 +1,6 @@
 git :init
 
 run "echo 'TODO add readme content' > README"
-run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
-run "cp config/database.yml config/example_database.yml"
-
-file ".gitignore", <<-END
-.bundle
-config/database.yml
-db/*.sqlite3
-log/*.log
-tmp/
-END
+run "touch tmp/.gitignore log/.gitignore"
 
 git :add => ".", :commit => "-m 'initial commit'"
